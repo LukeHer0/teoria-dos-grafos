f9cfd58dcf156b6992bb0a280442b412e4c5e28f
Ruan Melo <ruantmelo@gmail.com>
2023-10-12 02:26:31 -0300

    ajustando belman-ford e realizando mudanças iniciais na main

--- Dijkstra/dijkstra.cpp
+++ Dijkstra/dijkstra.cpp
@@ -36,3 +36,82 @@ vector<int> dijkstra(int source, int n, vector<vector<pair<int, int>>> &adj)
 
     return distance;
 }
+
+int main(int argc, char *argv[])
+{
+    string in = "";
+    string out = "";
+    int initial = 1;
+
+    for (int i = 1; i < argc; i++){
+        if (strcmp(argv[i], "-h") == 0){
+            cout << "Help:" << endl;
+            cout << "-h: mostra o help" << endl;
+            cout << "-o <arquivo>: redireciona a saida para o 'arquivo'" << endl;
+            cout << "-f <arquivo>: indica o 'arquivo' que contém o grafo de entrada" << endl;
+            cout << "-i: vértice inicial" << endl;
+            
+            return 0;
+        }
+        else if (strcmp(argv[i], "-o") == 0 && i < argc - 1){
+            out = argv[++i];
+        }
+        else if (strcmp(argv[i], "-f") == 0 && i < argc - 1){
+            in = argv[++i];
+        }
+        else if (strcmp(argv[i], "-i") == 0 && i < argc - 1){
+            initial = atoi(argv[++i]);
+        }
+    }
+
+    if (in == ""){
+        cerr << "No input file specified. Use the -f parameter." << endl;
+        return 1;
+    }
+
+    ifstream fin(in);
+
+    if (not fin){
+        cerr << "Could not open input file: " << in << endl;
+        return 1;
+    }
+
+    int V, E;
+    fin >> V >> E;
+
+    int a, b, cost;
+    vector<vector<pair<int, int>>> arestas(V + 1);
+
+    for (int i = 0; i < E; i++){
+        fin >> a >> b >> cost;
+        arestas[b].push_back({a, cost});
+        arestas[a].push_back({b, cost});
+    }
+
+    fin.close();
+
+    vector<int> distances = dijkstra(initial, V, arestas);
+
+    if (not (out == "")){
+        ofstream fout(out);
+        if (not fout){
+            cerr << "Could not open output file: " << out << endl;
+            return 1;
+        }
+        
+        for (int i = 1; i <= V; ++i) {
+            fout << i << ":" << distances[i] << " ";
+        }
+        fout << endl;
+
+        fout.close();
+    }
+
+    for (int i = 1; i <= V; ++i) {
+        cout << i << ":" << distances[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
