09ed5b9eaeac7bf735fdf3684e9112cb75050f40
Lucas Heron <111458155+LukeHer0@users.noreply.github.com>
2023-10-12 00:46:58 -0300

    Update bellman-ford.cpp
    checando se o grafo possui ciclos negativos

--- Bellman-ford/bellman-ford.cpp
+++ Bellman-ford/bellman-ford.cpp
@@ -4,34 +4,57 @@ using namespace std;
 
 typedef pair<pair<int, int>, int> Edge;
 
-vector<int> bellman_ford(int source, int n, vector<Edge> &edges){
-    vector<int> distance(n + 1, INT_MAX);
-    vector<int> visited(n+1, 0);
+int bellman_ford(int source, int n, int m, vector<int> &distance, vector<Edge> &edges){
+    
+    // vector<int> visited(n+1, 0);
     
 
     distance[source] = 0; 
-
-    for (int i = 1; i <= n - 1; i++ )
+    
+    for (int i = 1; i <= n; i++)
     {
+        // cout << "Fazendo " << i << " passo" << endl;
         bool changed = false;
         
         for (auto e : edges)
         {
+            
             pair<int, int> vertices = e.first;
             int weight = e.second;
             int src = vertices.first;
             int dst = vertices.second;
 
-            if (distance[dst] > distance[src] + weight)
+            if (distance[src] != INT_MAX && distance[dst] > (distance[src] + weight))
             {
+                // cout << "O dist do vértice " << dst << "atual é: " << distance[dst] << ' ';
                 changed = true;
                 distance[dst] = distance[src] + weight;
+                // cout << " mudou para: " << distance[dst] << ' ';
+                // cout << weight << ' ' << distance[src] << endl;
             }
         }
-        if (not changed) return distance;   
+
+        if (!changed) 
+        {
+            // cout << "Nenhuma alteração feita, parando o algoritmo" << endl;
+            return 0;
+        }   
+    }
+    for (auto e : edges)
+    {
+        pair<int, int> vertices = e.first;
+        int weight = e.second;
+        int src = vertices.first;
+        int dst = vertices.second;
+
+        if (distance[src] != INT_MAX && distance[dst] > (distance[src] + weight))
+        {
+            cout << "Grafo contém ciclo negativo!" << endl;
+            return -1;
+        }
     }
     
-    return distance;
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -78,7 +101,8 @@ int main(int argc, char *argv[])
 
     int a, b, cost;
     vector<Edge> arestas(V + 1);
-
+    
+    
     for (int i = 0; i < E; i++){
         fin >> a >> b >> cost;
         arestas.push_back({{a, b}, cost});
@@ -87,8 +111,12 @@ int main(int argc, char *argv[])
 
     fin.close();
 
-    vector<int> distances = bellman_ford(initial, V, arestas);
+    vector<int> distances(V + 1, INT_MAX);
 
+    int success = bellman_ford(initial, V, E, distances, arestas);
+
+    if (success == -1) return 0;
+    
     if (not (out == "")){
         ofstream fout(out);
         if (not fout){
@@ -103,10 +131,11 @@ int main(int argc, char *argv[])
 
         fout.close();
     }
-
+    
     for (int i = 1; i <= V; ++i) {
         cout << i << ":" << distances[i] << " ";
     }
+
     cout << endl;
 
     return 0;
